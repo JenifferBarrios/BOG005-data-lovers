013b01c214eef5d6abc0fbac185645afdfc5c586
alexandrettaBit <alxsalaza@gmail.com>
2022-08-11 22:21:48 -0500

    codigo con estilos CSS de la pagina organizados y responsive

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-import data from './data/ghibli/ghibli';
+import data from './data/ghibli/ghibli.js';
 
 console.log(example, data);
 
