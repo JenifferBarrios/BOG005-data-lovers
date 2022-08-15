34fa53ac3d675d9169f43190ad128ed8ae7c697c
JenifferBarrios <jbarrios03@hotmail.com>
2022-08-14 19:58:15 -0500

    menu hamburguesa responsive

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 console.log(example, data);
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+navToggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu_visible");
+});
 
 
 
