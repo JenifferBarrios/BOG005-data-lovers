4684d0117e67ced635e1377d9e82f8c6af4960aa
JenifferBarrios <jbarrios03@hotmail.com>
2022-08-17 18:10:01 -0500

    OH con David

--- src/main.js
+++ src/main.js
@@ -12,7 +12,20 @@ navToggle.addEventListener("click", () =>{
 });
 //console.log(data.films);
 console.log(data.films[0].title);
+const card1= document.querySelector(".card");
+const card1title=card1.querySelector("h2");
+console.log(card1title);
+card1title.textContent=data.films[0].title;
 
+const card2 = document.createElement('article')
+
+card2.innerHTML = `
+    <div style="background-image: url(${data.films[1].poster})" id="card-1"></div>
+    <h2>${data.films[1].title}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
+    <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
+`
+document.querySelector('.main').append(card2)
 
 
 
