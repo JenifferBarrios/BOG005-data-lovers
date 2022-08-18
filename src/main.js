4cc154105068c95f7e0bcbbec84dfd4820e21390
alexandrettaBit <alxsalaza@gmail.com>
2022-08-18 16:16:42 -0500

    Tiene el codigo completo con las tarjetas desde el template string

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 import data from './data/ghibli/ghibli.js';
+console.log(data.films[0].title);
+crearTarjetas(data.films);
 
 console.log(example, data);
 const navToggle = document.querySelector(".nav-toggle");
@@ -10,8 +8,18 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle("nav-menu_visible");
 });
-//console.log(data.films);
-console.log(data.films[0].title);
 
-//console.log(data.films);
-console.log(data.films[0].title);
+function crearTarjetas(peliculas){
+    const container = document.querySelector('.container')
+    peliculas.forEach(films => {
+        
+    container.innerHTML += `
+    <article class="card">
+            <div class="card-image" id="card-1"></div>
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
+            <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
+          </article>
+    `});
+    console.log(data.films);
+}
