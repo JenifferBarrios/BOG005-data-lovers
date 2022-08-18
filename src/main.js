55ef4f1e1feed38a2118c0834519a9b47c5a8a1e
JenifferBarrios <jbarrios03@hotmail.com>
2022-08-18 17:37:48 -0500

    este codigo imprime las 20 tarjetas con template

--- src/main.js
+++ src/main.js
@@ -1,32 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+//console.log(example, data);
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle("nav-menu_visible");
 });
-//console.log(data.films);
-console.log(data.films[0].title);
-const card1= document.querySelector(".card");
-const card1title=card1.querySelector("h2");
-console.log(card1title);
-card1title.textContent=data.films[0].title;
-
-const card2 = document.createElement('article')
-
-card2.innerHTML = `
-    <div style="background-image: url(${data.films[1].poster})" id="card-1"></div>
-    <h2>${data.films[1].title}</h2>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
-    <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
-`
-document.querySelector('.main').append(card2)
-
+crearTarjetas(data.films);
+function crearTarjetas(peliculas) {
+    const container=document.querySelector(".container")
+    peliculas.forEach(films => {
+    container.innerHTML += `
+    <article class="card">
+            <div class="card-image" id="card-1"></div>
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
+            <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
+          </article>
+    `});
+}
+console.log(data.films);
 
 
 
