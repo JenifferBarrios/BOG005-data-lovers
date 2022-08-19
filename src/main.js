7858b8c40c70e0cee9d803dea4faf2fc58bc4428
alexandrettaBit <alxsalaza@gmail.com>
2022-08-18 23:32:34 -0500

    Contiene las tarjetas con la informacion completa y los posters de la data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/ghibli/ghibli.js';
-console.log(data.films[0].title);
+// console.log(data.films[0].title);
 crearTarjetas(data.films);
 
 const navToggle = document.querySelector(".nav-toggle");
@@ -13,23 +13,23 @@ function crearTarjetas(peliculas){
     peliculas.forEach(films => {   
     container.innerHTML += `
     <article class="card">
-            <div class="card-image" id="card-1"></div>
-            <h2>Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
+            <img class="posters" src="${films.poster}"
+            <h2 class="titulo">${films.title}</h2>
+            <p>${films.description}</p>
             <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
           </article>
     `});
-    console.log(data.films);
+    // console.log(data.films);
 }
 //console.log(data.films);
-console.log(data.films[0].title);
+// console.log(data.films[0].title);
 
 //console.log(data.films);
-console.log(data.films[0].title);
+// console.log(data.films[0].title);
 
-const card1= document.querySelector(".card");
-const card1title=card1.querySelector("h2");
-console.log(card1title);
-card1title.textContent=data.films[0].title;
+// const card1= document.querySelector(".card");
+// const card1title=card1.querySelector("h2");
+// console.log(card1title);
+// card1title.textContent=data.films[0].title;
 
-const card2 = document.createElement('article')
+// const card2 = document.createElement('article')
