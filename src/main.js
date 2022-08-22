be89cd4810acc5f2ec1847d82e5f54fe6438b9cc
alexandrettaBit <alxsalaza@gmail.com>
2022-08-21 22:33:41 -0500

    intenta esconder la data de la segunda pantalla, aunque aun no lo logra

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import data from './data/ghibli/ghibli.js';
 // console.log(data.films[0].title);
 crearTarjetas(data.films);
+infoMovie(data.films);
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () =>{
@@ -13,17 +14,26 @@ function crearTarjetas(peliculas){
     <article class="card">
             <h2 class="titulo">${films.title}</h2>
             <img class="posters" src="${films.poster}"
-            <p class="descripcion">${films.description}</p>
-            <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
+            <button><a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a></button>
           </article>
     `});
     console.log(data.films);
 }
-//console.log(data.films);
-// console.log(data.films[0].title);
-// ***CODIGO PARA INSERTAR DATA EN EL DOM***
-// const card1= document.querySelector(".card");
-// const card1title=card1.querySelector("h2");
-// console.log(card1title);
-// card1title.textContent=data.films[0].title;
-// const card2 = document.createElement('article')
+function infoMovie(info){
+    const infoMovies = document.querySelector('.info-movies')
+    info.forEach(films =>{
+        infoMovies.innerHTML += `
+        <article class="paragraph">
+            <h2 class="titulo2">${films.title}</h2>
+            <p class="descripcion2"></p>
+            <p> ${films.description}</p>
+            <p>${films.director}</p>
+            <p>${films.producer}</p>
+             `
+    })
+}
+function mostrarInfo(){
+    document.getElementById('info-movies').style.display = 'block';
+}
+const movies = document.getElementById('movies-link');
+movies.addEventListener('click', mostrarInfo);
