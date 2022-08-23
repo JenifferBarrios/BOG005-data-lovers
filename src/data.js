a633a26e5f751e2f52cf79f88d82186edb9a7377
JenifferBarrios <jbarrios03@hotmail.com>
2022-08-23 10:48:31 -0500

    visualizacion ultimos cambios

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
 // estas funciones son de ejemplo
-
+import data from './data/ghibli/ghibli.js';
 export const example = () => {
   return 'example';
-};
+  };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { filtrarData } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 //console.log(example, data);
@@ -8,19 +9,31 @@ navToggle.addEventListener("click", () =>{
 });
 crearTarjetas(data.films);
 function crearTarjetas(peliculas) {
+    console.log(peliculas);
     const container=document.querySelector(".container")
     peliculas.forEach(films => {
     container.innerHTML += `
     <article class="card">
-            <div class="card-image" id="card-1"></div>
-            <h2>Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, laboriosam?</p>
+            <div class="card-image" id="card-1"> </div>
+            <h2>${films.title}</h2>
+            <p>${films.description}</p>
             <a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a>
           </article>
     `});
+
 }
 console.log(data.films);
+console.log(filtrarData);
 
+// Filtrar directores:
+/* const select = document.getElementById('director');//identifica al elemento 'director'
+select.addEventListener('change', (item) => {
+    // filtrarData();
+    const filtrarData = data.films.filter(dato => dato.director == item.target.value)
+console.log(filtrarData);  
+}) */
+const select = document.getElementById('director');
+select.addEventListener('change',item)
 
 
 
