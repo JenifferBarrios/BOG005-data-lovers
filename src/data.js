b64348728b16d9f8a1b49a5f3c1a5adb7f73ea96
alexandrettaBit <alxsalaza@gmail.com>
2022-08-23 17:35:53 -0500

    arregla un import que faltaba

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
+import data from './data/ghibli/ghibli.js';
 // *******Esta función filtra la data por director*******
 export function filtrarDirector(event){
   const filtrarData = data.films.filter(dato => dato.director == event.target.value)
+  console.log(filtrarData);
 }
 /* sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro,
  data, nos entrega los datos.
@@ -10,6 +12,3 @@ El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendent
 /* computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para
  ser mostrados de acuerdo a la data proporcionada.
  */
-
-
-
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import data from './data/ghibli/ghibli.js';
 import { filtrarDirector } from './data.js';
-// console.log(data.films[0].title);
 // *******Despliega menú hamburguesa*******
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
@@ -18,9 +17,8 @@ function crearTarjetas(peliculas){
             <button><a href="https://www.ghiblicollection.com/product/my-neighbor-totoro?product_id=6644" target="_blank">Conoce Mas</a></button>
           </article>
     `});
-    console.log(data.films);
 }
-crearTarjetas(data.films);
+crearTarjetas(data.films);//llama a la function crearTarjetas
 
 // ***Imprime la info completa *** actualmente sin estilo y escondida.
 function infoMovie(info){
@@ -35,15 +33,9 @@ function infoMovie(info){
             <p>${films.producer}</p>
     `})
 }
-infoMovie(data.films);
-// Mostrar la info completa en el DOM aún no funciona
-/* function mostrarInfo(){
-    document.getElementById('info-movies').style.display = 'block';
-}
-const movies = document.getElementById('movies-link');
-movies.addEventListener('click', mostrarInfo); */
+infoMovie(data.films);//llama a la function infoMovie
 
 // *******Filtrar directores*******
 const select = document.getElementById('director');//identifica al elemento 'director'
-select.addEventListener('change', filtrarDirector);
+select.addEventListener('change', filtrarDirector);//determina el evento.
 
