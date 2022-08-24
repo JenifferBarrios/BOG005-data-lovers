7941e461fb7e56dc68a6164a3f12ab675392c12d
alexandrettaBit <alxsalaza@gmail.com>
2022-08-23 21:56:30 -0500

    contiene el codigo ordenado y funcionando hasta la funcion filtrarDirector

--- src/data.js
+++ src/data.js
@@ -1,7 +1,9 @@
 import data from './data/ghibli/ghibli.js';
 // *******Esta función filtra la data por director*******
 export function filtrarDirector(event){
-  const filtrarData = data.films.filter(dato => dato.director == event.target.value)
+  const filtrarData = data.films.filter(function(datoDirector){
+    return datoDirector.director === event.target.value;
+  });
   console.log(filtrarData);
 }
 /* sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro,
@@ -9,6 +11,6 @@ export function filtrarDirector(event){
 El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. 
 El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente. */
 
+
 /* computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para
- ser mostrados de acuerdo a la data proporcionada.
- */
+ ser mostrados de acuerdo a la data proporcionada.*/
